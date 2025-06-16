e5daa298ac7837fa37e22feb5e7170551855b619
MNourMoslem <normoslem256@gmail.com>
2025-06-17 00:36:27 +0300

    init mobile app

---
+++ mobile/TeamWorkMobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./global.css"; // Ensure global styles are imported
+
+export default function Index() {
+  return (
+    <View>
+      <Text>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
